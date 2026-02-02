ed7b62307f79e32b16a551d305128d1baa671135
jjminsuh <minsuh3203@gmail.com>
2026-02-02 20:00:35 +0900

    feat: 클릭한 노드에 focusing 하는 기능 추가

--- app/curriculum/[id]/_components/GraphCanvasNew.tsx
+++ app/curriculum/[id]/_components/GraphCanvasNew.tsx
@@ -45,18 +45,18 @@ export default function GraphCanvas(props: GraphCanvasProps) {
     console.log(props.nodePositions);
 
     const mappedNodes = props.nodes.map((node, index) => ({
-      id: node.keyword_id, // React Flow는 string id 필수
+      id: node.keyword_id,
       type: "curriculum", // or "input", "output", custom node
       position: props.nodePositions[node.keyword_id],
-      //   position: { x: 0, y: 0 },
       data: {
         label: node.keyword,
-        curriculum: node, // 👈 CurriculumNode 통째로 넣기
+        curriculum: node,
       },
+      selected: node.keyword_id === props.selectedNodeId,
     }));
 
     const mappedEdges = props.edges.map((edge, index) => ({
-      id: `e-${edge.start}-${edge.end}`, // 🔑 유니크
+      id: `e-${edge.start}-${edge.end}`,
       source: edge.start,
       target: edge.end,
       type: "default", // or "default", "step", "bezier"
@@ -71,7 +71,14 @@ export default function GraphCanvas(props: GraphCanvasProps) {
 
     setNodes(mappedNodes);
     setEdges(mappedEdges);
-  }, [props.nodes, props.edges, props.nodePositions, setNodes, setEdges]);
+  }, [
+    props.nodes,
+    props.edges,
+    props.nodePositions,
+    props.selectedNodeId,
+    setNodes,
+    setEdges,
+  ]);
 
   return (
     <div className={`w-full h-full`}>
@@ -82,11 +89,15 @@ export default function GraphCanvas(props: GraphCanvasProps) {
         edges={rfedges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
+        onNodeClick={(_, node) =>
+          props.onNodeSelect(node.data.curriculum as CurriculumNode)
+        }
         nodeTypes={nodeTypes}
         attributionPosition='bottom-left'
       >
         <Controls />
         <FitViewOnLoad nodes={rfnodes} />
+        <FocusOnNode selectedNodeId={props.selectedNodeId} />
       </ReactFlow>
     </div>
   );
@@ -102,6 +113,29 @@ function FitViewOnLoad({ nodes }: { nodes: Node[] }) {
       reactFlow.fitView({ padding: 0.2 });
     });
   }, [nodes.length, reactFlow]);
+
+  return null;
+}
+
+function FocusOnNode({ selectedNodeId }: { selectedNodeId: string | null }) {
+  const { setCenter, getZoom, getNode } = useReactFlow();
+
+  useEffect(() => {
+    if (!selectedNodeId) return;
+
+    const node = getNode(selectedNodeId);
+    if (!node) return;
+
+    const width = node.measured?.width ?? 0;
+    const height = node.measured?.height ?? 0;
+
+    const x = node.position.x + width / 2;
+    const y = node.position.y + height / 2;
+
+    const zoom = getZoom();
+
+    setCenter(x, y, { zoom, duration: 1000 });
+  }, [selectedNodeId, getNode, setCenter, getZoom]);
 
   return null;
 }
